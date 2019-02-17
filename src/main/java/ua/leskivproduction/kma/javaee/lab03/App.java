0cf3c59adebf704ffde5afb6be75aebda5863196
Oleksandr Leskiv <sasha26.05.2012@gmail.com>
2019-02-17 19:19:47 +0200

    initial commit

---
+++ src/main/java/ua/leskivproduction/kma/javaee/lab03/App.java
@@ -0,0 +1,15 @@
+package ua.leskivproduction.kma.javaee.lab03;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ua.leskivproduction.kma.javaee.lab03.catalogue.Catalogue;
+
+public class App {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+        Catalogue catalogue = context.getBean(Catalogue.class);
+        System.out.println(catalogue);
+    }
+
+}
